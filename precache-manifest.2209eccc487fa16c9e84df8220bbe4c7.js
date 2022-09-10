3c6c7fa11a7e148e36d8eaa1958a1a906f9fcbe9
Chialiang Chou <arthur@iswind.com.tw>
2022-09-10 19:52:17 +0800

    Updates

---
+++ precache-manifest.2209eccc487fa16c9e84df8220bbe4c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a12fdedae2adce4d2a10ac4617d35a",
+    "url": "/bomfilter/index.html"
+  },
+  {
+    "revision": "8c5c57795eb762fbb811",
+    "url": "/bomfilter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ff9760393ce171a78f58",
+    "url": "/bomfilter/static/js/2.eeafaca6.chunk.js"
+  },
+  {
+    "revision": "669d6dff4309c3a5e8944f92919ac3db",
+    "url": "/bomfilter/static/js/2.eeafaca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5c57795eb762fbb811",
+    "url": "/bomfilter/static/js/main.785e46bf.chunk.js"
+  },
+  {
+    "revision": "e692aff63306f7635562",
+    "url": "/bomfilter/static/js/runtime-main.1afe84a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bomfilter/static/media/logo.5d5d9eef.svg"
+  }
+]);
